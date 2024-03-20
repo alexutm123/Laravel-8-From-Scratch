a1495f67ebd475c5abe5cd2e306e7f72cb16757d
alexutm123 <pilich.sasha@gmail.com>
2024-03-20 21:47:55 +0300

    Lesson№7-Make a Route and Link to it

---
+++ example-app/resources/views/post.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="app.css">
+
+    <title>Document</title>
+</head>
+<body>
+<article>
+    <h1><a href="/post">My First Post</a></h1>
+    <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates pariatur beatae, saepe, aspernatur error voluptate, ullam odit sequi labore reprehenderit quos. Provident rerum veniam molestias repellat, non quis cum nihil?
+    </p>
+   </article>
+   <a href="/">Go back</a>
+</body>
+</html>
