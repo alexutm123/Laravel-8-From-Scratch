b45748392cd65c8fad153b35e5d25f4f4c188c51
alexutm123 <pilich.sasha@gmail.com>
2024-03-21 18:31:37 +0300

    Lesson№12-Find a Composer Package for Post Metadata

--- example-app/resources/views/posts.blade.php
+++ example-app/resources/views/posts.blade.php
@@ -1,18 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="app.css">
-   
-    <title>MY Blog</title>
+
+    <title>My Blog</title>
 </head>
+
 <body>
-    <?php foreach($posts as $post) :?>
-   <article>
-<?= $post ?>
-   
-   </article>
-  <?php endforeach; ?>
+    <?php foreach ($posts as $post) : ?>
+        <article>
+            <h1>
+                <a href="/posts/<?= $post->slug; ?>">
+                    <?= $post->title; ?>
+                </a>
+            </h1>
+            <div>
+                <?= $post->excerpt; ?>
+            </div>
+        </article>
+    <?php endforeach; ?>
 </body>
+
 </html>
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,13 +1,34 @@
 <?php
 
 use App\Models\Post;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', function () {
+ 
+
+
+    // $posts=array_map(function($file)
+    // {
+    //     $document=YamlFrontMatter::parseFile($file);
+
+    //     return new Post(
+    //         $document->title,
+    //         $document->excerpt,
+    //         $document->date,
+    //         $document->slug,
+    //         $document-> body(),
+            
+    //     );
+    // },$files);
+   
     
+
     return view('posts',[
         'posts'=>Post::all()
     ]);
+   
 });
 
 
