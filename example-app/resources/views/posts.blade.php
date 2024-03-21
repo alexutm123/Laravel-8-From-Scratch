761b09a7967d6ba561d8f4bd915d6d2362a9d221
alexutm123 <pilich.sasha@gmail.com>
2024-03-21 14:19:33 +0300

    Lesson№11-Use the Filesystem Class to Read a Directory

--- example-app/resources/views/posts.blade.php
+++ example-app/resources/views/posts.blade.php
@@ -5,26 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="app.css">
    
-    <title>Document</title>
+    <title>MY Blog</title>
 </head>
 <body>
+    <?php foreach($posts as $post) :?>
    <article>
-    <h1><a href="/posts/my-first-post">My First Post</a></h1>
-    <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates pariatur beatae, saepe, aspernatur error voluptate, ullam odit sequi labore reprehenderit quos. Provident rerum veniam molestias repellat, non quis cum nihil?
-    </p>
+<?= $post ?>
+   
    </article>
-   <article>
-   <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-    <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates pariatur beatae, saepe, aspernatur error voluptate, ullam odit sequi labore reprehenderit quos. Provident rerum veniam molestias repellat, non quis cum nihil?
-    </p>
-   </article>
-   <article>
-   <h1><a href="/posts/My-third-post">My Third Post</a></h1>
-    <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates pariatur beatae, saepe, aspernatur error voluptate, ullam odit sequi labore reprehenderit quos. Provident rerum veniam molestias repellat, non quis cum nihil?
-    </p>
-   </article>
+  <?php endforeach; ?>
 </body>
 </html>
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,22 +1,18 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('posts');
-});
-Route::get('posts/{post}', function ($slug) {
     
-    if(!file_exists($path=__DIR__ . "/../resources/posts/{$slug}.html"))
-    {
-        return redirect('/');
-    //    abort(404);
-    }
-    $post=cache()->remember("posts.{$slug}", 1200, function() use ($path)
-    {
-        var_dump('file_get_contents');
-        return file_get_contents($path);
-    });
-    $post=file_get_contents($path);
-    return view('post',['post'=>$post]);
+    return view('posts',[
+        'posts'=>Post::all()
+    ]);
+});
+
+
+Route::get('posts/{post}', function ($slug) {
+
+    return view('post',['post'=>Post::find($slug)
+]);
 })->where('post','[A-z_\-]+');
