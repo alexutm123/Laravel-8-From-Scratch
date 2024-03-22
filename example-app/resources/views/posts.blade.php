b2bf110c6c16ad8af5d395305351e7691d1f7aa9
alexutm123 <pilich.sasha@gmail.com>
2024-03-22 10:47:57 +0300

    Lesson№15-Blade Layouts Two Ways

--- example-app/resources/views/posts.blade.php
+++ example-app/resources/views/posts.blade.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="app.css">
-
-    <title>My Blog</title>
-</head>
-
-<body>
+<x-layout >
+<x-slot name="content">
  @foreach ($posts as $post) 
+
  <article class="{{ $loop->even ? 'foobar' : '' }}">
 
             <h1>
@@ -23,6 +14,5 @@
             </div>
         </article>
     @endforeach
-</body>
-
-</html>
+    </x-slot>
+    </x-layout>
