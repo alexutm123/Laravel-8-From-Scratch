b85f36079b9596f2f85dd339ae532fcdd4f377e1
alexutm123 <pilich.sasha@gmail.com>
2024-03-23 11:20:55 +0300

    Lesson№22-3 Ways to Mitigate Mass Assignment Vulnerabilities

--- example-app/resources/views/post.blade.php
+++ example-app/resources/views/post.blade.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 <article>
-<h1>{{$post->title;}}</h1>
+<h1>{!!$post->title!!}</h1>
 <div>
     {!!$post->body;!!}
 </div>
--- example-app/resources/views/posts.blade.php
+++ example-app/resources/views/posts.blade.php
@@ -6,7 +6,7 @@
 
             <h1>
                 <a href="/posts/{{$post->id;}}">
-                    {{ $post->title;}}
+                    {!! $post->title!!}
                 </a>
             </h1>
             <div>
